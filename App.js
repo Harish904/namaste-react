34a9afd23de36d2ecd32e62eb73efbb7f49f818f
Harish904 <harish.banti9044@gmail.com>
2025-10-09 17:44:49 +0530

    Overview on Functional Components

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => <h1 className="head"> Namaste React using JSX</h1>;
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading">Namaste React using Functional Component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
