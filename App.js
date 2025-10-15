6ad5bfa65abfc818cac4dc609c15aa441a91fee3
Harish904 <harish.banti9044@gmail.com>
2025-10-15 14:55:27 +0530

    Small Project on React Props & Functional Components

--- App.js
+++ App.js
@@ -1,15 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => <h1 className="head"> Namaste React using JSX</h1>;
-const HeadingComponent = () => (
-  <div id="container">
-    {Title()}
-    <Title />
-    <Title></Title>
-    <h1 className="heading">Namaste React using Functional Component</h1>
-  </div>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.shutterstock.com/image-vector/vector-neon-burger-logo-template-600nw-1648319593.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="res-logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS15Q4QnWXaTQWMbNzUA6f4Li8QZw5P5pOFA&s"
+        alt="res-logo"
+      ></img>
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine}</h4>
+      <h4>4.4 stars</h4>
+      <h4>30 mins</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard
+          resName="Meghana Foods"
+          cuisine="Biriyani, South Indian"
+        />
+        <RestaurantCard resName="KFC" cuisine="Burgers, French Fries" />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
