9eca3edbda5b0b5bf111e94c5b3e6e2d665193ac
Harish904 <harish.banti9044@gmail.com>
2025-10-21 17:45:00 +0530

    Swiggy API

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const LOGO_URL =
+  "https://www.shutterstock.com/image-vector/vector-neon-burger-logo-template-600nw-1648319593.jpg";
+
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
